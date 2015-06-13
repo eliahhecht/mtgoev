6d7b6487b9eb1a6bb585ca3106f95ab85aa7d725
Eliah Hecht <eliahhecht@gmail.com>
2015-06-13 16:30:19 -0400

    Add pack EV calculation

--- main/set.go
+++ main/set.go
@@ -1,5 +1,31 @@
 package main
 
+import "github.com/wayn3h0/go-decimal"
+
 type Set struct {
 	Cards []Card
+}
+
+func (s *Set) PackEV() *decimal.Decimal {
+	avgCommonPrice := s.getAvgPrice(RARITY_C)
+	avgUncommonPrice := s.getAvgPrice(RARITY_U)
+	avgRarePrice := s.getAvgPrice(RARITY_R)
+	avgMythicPrice := s.getAvgPrice(RARITY_M)
+
+	return decimal.New(11).Mul(avgCommonPrice).
+		Add(decimal.New(3).Mul(avgUncommonPrice)).
+		Add(decimal.New(7 / 8.0).Mul(avgRarePrice)).
+		Add(decimal.New(1 / 8.0).Mul(avgMythicPrice))
+}
+
+func (s *Set) getAvgPrice(rarity int) *decimal.Decimal {
+	sum := decimal.New(0)
+	count := 0.0
+	for _, card := range s.Cards {
+		if card.Rarity == rarity {
+			count++
+			sum = sum.Add(card.Price)
+		}
+	}
+	return sum.Div(decimal.New(count))
 }
---
+++ main/set_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/wayn3h0/go-decimal"
+	"testing"
+)
+
+func makeCard(rarity int, price float64) Card {
+	return Card{Rarity: rarity, Price: decimal.New(price)}
+}
+
+func TestSetEvCalculatesCorrectly(t *testing.T) {
+	set := Set{
+		Cards: []Card{
+			makeCard(RARITY_C, 1),
+			makeCard(RARITY_C, 0),
+			makeCard(RARITY_U, 2),
+			makeCard(RARITY_R, 3),
+			makeCard(RARITY_M, 4),
+			makeCard(RARITY_BASIC, 99)}}
+
+	ev := set.PackEV()
+
+	assert.Equal(t, decimal.New(14.625), ev, "The EV of a pack should be calculated correctly")
+}
