c34d74dee218fd9f29dd73512ccc501df5c5c889
Eliah Hecht <eliahhecht@gmail.com>
2015-06-14 20:53:55 -0400

    add test coverage for 8-4s

--- main/event_test.go
+++ main/event_test.go
@@ -36,3 +36,46 @@ func TestFlatPrizeDistroComputesEVCorrectly(t *testing.T) {
 	// expected prizes: 1 pack
 	assert.Equal(t, -1.0, ev, "The EV of the event should be computed properly")
 }
+
+type EightFourPrizeDistro struct{}
+
+func (_ EightFourPrizeDistro) GetPrizesForPlace(place int) *decimal.Decimal {
+	switch place {
+	case 1:
+		return decimal.New(8.0)
+	case 2:
+		return decimal.New(4.0)
+	default:
+		return decimal.New(0.0)
+	}
+}
+
+func TestEightFourComputesEVCorrectlyFor50PctWin(t *testing.T) {
+	testPack := GetTestPack()
+	event := Event{
+		EntryPacks:  []Pack{testPack, testPack, testPack},
+		EntryFeeTix: 2,
+		Prizes:      EightFourPrizeDistro{}}
+
+	ev := event.EV(0.5)
+
+	// cost: 3 packs + 2 tix = 5
+	// voc: 3 packs * 1 = 3
+	// expected prizes: 1.5 packs
+	assert.Equal(t, -0.5, ev, "The EV of the event should be computed properly")
+}
+
+func TestEightFourComputesEVCorrectlyFor100PctWin(t *testing.T) {
+	testPack := GetTestPack()
+	event := Event{
+		EntryPacks:  []Pack{testPack, testPack, testPack},
+		EntryFeeTix: 2,
+		Prizes:      EightFourPrizeDistro{}}
+
+	ev := event.EV(1)
+
+	// cost: 3 packs + 2 tix = 5
+	// voc: 3 packs * 1 = 3
+	// expected prizes: 8 packs
+	assert.InEpsilon(t, 6, ev, 0.001, "The EV of the event should be computed properly")
+}
